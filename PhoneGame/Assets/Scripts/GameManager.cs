cc7640075deefdcdb3a4bf414db0d84cbe9e8ece
bfc6072 <bfc6072@g.rit.edu>
2018-12-11 15:41:36 -0500

    Added in music, Player identification, and Card removal so players don't get two of the same challenges

--- PhoneGame/Assets/Scripts/GameManager.cs
+++ PhoneGame/Assets/Scripts/GameManager.cs
@@ -6,10 +6,16 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
     public static GameManager instance = null;
     public Challenges challenges;
+    public GameObject RemovalScreen;
+    public GameObject QRCodeScreen;
     [SerializeField]
     Text gameName;
     [SerializeField]
     Text instructions;
+    [SerializeField]
+    Text playerText;
+    [SerializeField]
+    Text removalText;
 
     void Start () {
         //Instance gamemanager on start
@@ -28,34 +34,61 @@ public class GameManager : MonoBehaviour {
 
     public void SelectGame(string selection)
     {
-        int game = Random.Range(1, 4);
         int range = 0;
         string[] data;
         switch (selection)
         {
-            case "onevone":
+            case "http://onevsone":
+                if(challenges.OnevOne.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all One V One cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.OnevOne.Count);
                 data = challenges.OnevOne[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(1);
+                challenges.OnevOne.RemoveAt(range);
                 break;
-            case "twovtwo":
+            case "http://twovstwo":
+                if (challenges.TwovTwo.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all Two V Two cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.TwovTwo.Count);
                 data = challenges.TwovTwo[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(2);
+                challenges.TwovTwo.RemoveAt(range);
                 break;
-            case "onevall":
+            case "http://onevsall":
+                if (challenges.OnevAll.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all One V All cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.OnevAll.Count);
                 data = challenges.OnevAll[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(0);
+                challenges.OnevAll.RemoveAt(range);
                 break;
-            case "royale":
+            case "http://royale":
+                if (challenges.Royale.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all Royale cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.Royale.Count);
-                data = challenges.Royale[0].Split('|');
+                data = challenges.Royale[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(0);
+                challenges.Royale.RemoveAt(range);
                 break;
             default:
                 RandomChallenge();
@@ -67,35 +100,110 @@ public class GameManager : MonoBehaviour {
     {
         int range = Random.Range(0, 4);
         string[] data;
+
         switch (range)
         {
             case 0:
+                if (challenges.OnevOne.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all One V One cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.OnevOne.Count);
                 data = challenges.OnevOne[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(1);
+                challenges.OnevOne.RemoveAt(range);
                 break;
             case 1:
+                if (challenges.TwovTwo.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all Two V Two cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.TwovTwo.Count);
                 data = challenges.TwovTwo[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(2);
+                challenges.TwovTwo.RemoveAt(range);
                 break;
             case 2:
+                if (challenges.OnevAll.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all One V All cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.OnevAll.Count);
                 data = challenges.OnevAll[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(0);
+                challenges.OnevAll.RemoveAt(range);
                 break;
             case 3:
+                if (challenges.Royale.Count == 0)
+                {
+                    DisplayChallengeRemoval("Remove all Royale cards. \n DON'T PLACE THEM BACK IN PILE");
+                    return;
+                }
                 range = Random.Range(0, challenges.Royale.Count);
-                data = challenges.Royale[0].Split('|');
+                data = challenges.Royale[range].Split('|');
                 gameName.text = data[0];
                 instructions.text = data[1];
+                ChoosePlayer(0);
+                challenges.Royale.RemoveAt(range);
                 break;
             default:
                 Debug.Log("Error: Data isn't able to be parse and presented to the user");
                 break;
+        }
+    }
+
+    /// <summary>
+    /// Sets the text field of the Removal Screen so the player knows what cards to remove
+    /// Enables and Disables correct screens
+    /// </summary>
+    /// <param name="text"></param>
+    private void DisplayChallengeRemoval(string text)
+    {
+        removalText.text = text;
+        QRCodeScreen.SetActive(false);
+        RemovalScreen.SetActive(true);
+    }
+
+    private void ChoosePlayer(int selection)
+    {
+        if(selection == 1)
+        {
+            int player = Random.Range(1, 4);
+            int random = Random.Range(0, 1);
+            if(random == 0)
+            {
+                playerText.text = "You (Phone Holder) and the player " + player + " clockwise from you";
+            }
+            else
+            {
+                playerText.text = "You (Phone Holder) and the player " + player + " counter-clockwise from you";
+            }
+        }
+        else if (selection == 2)
+        {
+            int player = Random.Range(1, 4);
+            int random = Random.Range(0, 1);
+            if (random == 0)
+            {
+                playerText.text = "You (Phone Holder) and the player " + player + " clockwise from you are a Team";
+            }
+            else
+            {
+                playerText.text = "You (Phone Holder) and the player " + player + " counter-clockwise from you are a Team";
+            }
+        }
+        else
+        {
+            playerText.text = "";
         }
     }
 
@@ -132,4 +240,6 @@ public class Challenges
         Royale = new List<string>();
     }
 }
+
+
 
--- PhoneGame/Assets/Scripts/QR_Code.cs
+++ PhoneGame/Assets/Scripts/QR_Code.cs
@@ -10,14 +10,13 @@ public class QR_Code : MonoBehaviour {
 
     private WebCamTexture camTexture;
     private Rect screenRect;
-    public Text text;
     public GameObject qrScreen;
     public GameObject challengeScreen;
     public Transform qrLocation;
     private Vector2 size = new Vector2(1000, 1000);
     Vector2 pos = new Vector2(Screen.height * .25f, Screen.width * .25f);
     Vector2 pivot;
-    public float angle = 90;
+    float angle = 90;
 
     void Start()
     {
@@ -58,7 +57,6 @@ public class QR_Code : MonoBehaviour {
             {
                 Debug.Log("DECODED TEXT FROM QR: " +result.Text);
                 GameManager.instance.SelectGame(result.Text);
-                text.text = result.Text;
                 qrScreen.SetActive(false);
                 challengeScreen.SetActive(true);
             }
